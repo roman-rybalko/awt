c3ca78f9f880dd7cfd065da1f2f6f768bf8e7e3f
Roman Rybalko <devel@romanr.info>
2015-08-09 11:49:51 +0300

    config upd to chrome

--- client/config.js
+++ client/config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	server_url: 'http://www/awt/server/task.php',
 	server_token: 'EtZGlOGWMGtEOptUcaQN98KTnPrXpvXgpY1orOue04',
-	task_type: 'ff',
+	task_type: 'gc',
 	node_id: 'test1',
-	selenium_browser: 'firefox',
+	selenium_browser: 'chrome',
 	selenium_server: 'http://localhost:4410/wd/hub',
 	selenium_port: 4410,
 	selenium_timeout: 10000,  // msec
