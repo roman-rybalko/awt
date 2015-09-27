d7dd2a237f270b2a6c27589ee27a9f9f6fbedefc
Roman Rybalko <devel@romanr.info>
2015-09-27 13:43:24 +0300

    db.sql generator fix

--- server/db.sql.sh
+++ server/db.sql.sh
@@ -1,4 +1,4 @@
 cat web_construction_set/database/relational/user.sql
-sed 's/anacron/task_schedule/' web_construction_set/database/relational/anacron.sql
+sed 's/anacron/task_schedule/g' web_construction_set/database/relational/anacron.sql
 cat web_construction_set/database/relational/history.sql
-sed 's/anacron/mail_schedule/' web_construction_set/database/relational/anacron.sql
+sed 's/anacron/mail_schedule/g' web_construction_set/database/relational/anacron.sql
