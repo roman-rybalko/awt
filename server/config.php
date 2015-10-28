a614e198582d57d9d36b436fccdc863c691aad28
Roman Rybalko <devel@romanr.info>
2015-10-28 18:18:44 +0300

    removed php closing tags in config

--- server/config.php
+++ server/config.php
@@ -4,5 +4,3 @@ if (file_exists(dirname(__FILE__) . '/config.local.php'))
 	require_once dirname(__FILE__) . '/config.local.php';
 else
 	require_once dirname(__FILE__) . '/config.sample.php';
-
-?>
--- server/config.sample.php
+++ server/config.sample.php
@@ -24,5 +24,3 @@ class Config {
 }
 
 \Config::$rootPath = __DIR__ . '/';
-
-?>
