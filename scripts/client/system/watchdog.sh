04c193add2dd8689ab9c2b3e0fb80b90600ca2ea
Roman Rybalko <devel@romanr.info>
2015-10-30 13:29:55 +0300

    scripts watchdog msg upd

--- scripts/client/system/watchdog.sh
+++ scripts/client/system/watchdog.sh
@@ -13,5 +13,5 @@ cd "`dirname "$config"`"
 if [ -n "$SINGLE_LOCK" ]; then
 	T1=0`stat -c %Y "$SINGLE_LOCK" 2>/dev/null`
 	T2=0`date -d "-$timeout_minutes minutes" +%s 2>/dev/null`
-	[ $T1 = 0 -o $T2 = 0 ] || [ $T1 -gt $T2 ] || { shutdown -r && echo "Shutdown scheduled, SINGLE_LOCK=$SINGLE_LOCK, T1=$T1, T2=$T2." ; }
+	[ $T1 = 0 -o $T2 = 0 ] || [ $T1 -gt $T2 ] || { shutdown -r && echo "Watchdog, SINGLE_LOCK=$SINGLE_LOCK, T1=$T1, T2=$T2." ; }
 fi
