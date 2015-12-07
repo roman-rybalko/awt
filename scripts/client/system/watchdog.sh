16be0f4a7edf4f9ec2a21100308bc88e16073770
Roman Rybalko <devel@romanr.info>
2015-12-07 19:55:29 +0300

    scripts: watchdog handle multiple locks

--- scripts/client/system/watchdog.sh
+++ scripts/client/system/watchdog.sh
@@ -1,17 +1,36 @@
 #! /bin/sh
 
 if [ -z "$1" ]; then
-	echo "USAGE: $0 <timeout_minutes> <config.sh>"
+	echo "USAGE: $0 <timeout_minutes> [config.sh] [config.sh] ..."
 	exit 1
 fi
+
 timeout_minutes=$1
-config="$2"
+shift
 
-cd "`dirname "$config"`"
-. ./"`basename "$config"`"
+pushd()
+{
+	oldd="`pwd`"
+	cd "$1"
+}
 
-if [ -n "$SINGLE_LOCK" ]; then
-	T1=0`stat -c %Y "$SINGLE_LOCK" 2>/dev/null`
-	T2=0`date -d "-$timeout_minutes minutes" +%s 2>/dev/null`
-	[ $T1 = 0 -o $T2 = 0 ] || [ $T1 -gt $T2 ] || { shutdown -r && echo "Watchdog, SINGLE_LOCK=$SINGLE_LOCK, T1=$T1, T2=$T2." ; }
-fi
+popd()
+{
+	cd "$oldd"
+	oldd=.
+}
+
+for config in "$@"; do
+	pushd "`dirname "$config"`"
+	. ./"`basename "$config"`"
+	if [ -n "$SINGLE_LOCK" ]; then
+		T1=0`stat -c %Y "$SINGLE_LOCK" 2>/dev/null`
+		T2=0`date -d "-$timeout_minutes minutes" +%s 2>/dev/null`
+		if [ $T1 != 0 -a $T2 != 0 -a $T1 -lt $T2 ]; then
+			shutdown -r
+			echo "Watchdog, SINGLE_LOCK=$SINGLE_LOCK, T1=$T1, T2=$T2."
+			exit 0
+		fi
+	fi
+	popd
+done
