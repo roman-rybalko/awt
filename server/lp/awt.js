ef0337baf69ccfd55eb8e374c7a2f5bb49489f42
Roman Rybalko <devel@romanr.info>
2015-12-15 04:17:33 +0300

    lp: mobile fixes

--- server/lp/awt.js
+++ server/lp/awt.js
@@ -20,35 +20,56 @@ function reformat() {
 }
 $(window).resize(reformat);
 
+var menuBarActive = false;
 var menuBarPinUp = false;
-var menuBarActive = 0;
-var menuBarItems = ['#menubar-item-description', '#menubar-item-use-cases', '#menubar-item-features', '#menubar-item-pricing'];
+var menuItemActive = 0;
+var menuItems = ['#menubar-item-description', '#menubar-item-use-cases', '#menubar-item-features', '#menubar-item-pricing'];
 function updateMenuBar() {
-	if (menuBarPinUp) {
+	if ($(window).width() < 964 && menuBarActive === true) {
+		$('#menubar').hide();
+		$('#menubar-placeholder').hide();
+		$('#menubar-mobile').show();
+		menuBarActive = false;
+	} else if ($(window).width() >= 964 && menuBarActive === false) {
+		$('#menubar').show();
+		$('#menubar-mobile').hide();
+		menuBarActive = null;
+	}
+	if (menuBarActive === null) {
 		if ($(window).scrollTop() < 110) {
 			$('#menubar').removeClass('awt-menubar-pinup');
 			$('#menubar-placeholder').hide();
 			menuBarPinUp = false;
+		} else if ($(window).scrollTop() >= 110) {
+			$('#menubar').addClass('awt-menubar-pinup');
+			$('#menubar-placeholder').show();
+			menuBarPinUp = true;
 		}
-	} else {
-		if ($(window).scrollTop() >= 110) {
+		menuBarActive = true;
+	} else if (menuBarActive) {
+		if ($(window).scrollTop() < 110 && menuBarPinUp) {
+			$('#menubar').removeClass('awt-menubar-pinup');
+			$('#menubar-placeholder').hide();
+			menuBarPinUp = false;
+		} else if ($(window).scrollTop() >= 110 && !menuBarPinUp) {
 			$('#menubar').addClass('awt-menubar-pinup');
 			$('#menubar-placeholder').show();
 			menuBarPinUp = true;
 		}
 	}
 	var active = 0;
-	for (active = 0; active < menuBarItems.length; ++active)
+	for (active = 0; active < menuItems.length; ++active)
 		if ($(window).scrollTop() + $(window).height() * 2 / 3 < menuBarPoss[active + 1])
 			break;
-	if (active != menuBarActive) {
-		for (var i in menuBarItems)
-			$(menuBarItems[i]).removeClass('active');
-		$(menuBarItems[active]).addClass('active');
-		menuBarActive = active;
+	if (active != menuItemActive) {
+		for (var i in menuItems)
+			$(menuItems[i]).removeClass('active');
+		$(menuItems[active]).addClass('active');
+		menuItemActive = active;
 	}
 }
 $(window).on('scroll', updateMenuBar);
+$(window).resize(updateMenuBar);
 
 $(function(){
 	reformat();
