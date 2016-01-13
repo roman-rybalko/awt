34ba7cd653a63ed032829b7fe6d4a3b66e3fed5f
Roman Rybalko <devel@romanr.info>
2016-01-13 03:23:05 +0300

    ui: xpath-browser-composer service script refactoring

--- server/ui/ui-en/php/xpath-browser-composer.php
+++ server/ui/ui-en/php/xpath-browser-composer.php
@@ -4,16 +4,13 @@ header('Content-Type: text/javascript');
 (function() {
 <?php
 readfile('../js/jquery.min.js');
-echo "\n";
 readfile('../js/messaging.js');
 ?>
 var messaging = new Messaging("server");
 messaging.ping();
 <?php
 readfile('../js/error-server.js');
-echo "\n";
 readfile('../js/xpath-browser-server.js');
-echo "\n";
 readfile('../js/xpath-composer-server.js');
 ?>
 $.noConflict(true);
