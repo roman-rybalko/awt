d9d468634b038d87a3f43dbf004d72d5c67da9e7
Roman Rybalko <devel@romanr.info>
2016-01-21 16:39:45 +0300

    lp: ui preloader

--- server/advanced_web_testing/demo.php
+++ server/advanced_web_testing/demo.php
@@ -15,21 +15,6 @@ class Demo {
 		if ($user->getId())
 			$user->logout();
 		$user->login('', null);
-		$url = './';
-		if (!empty($_SERVER['QUERY_STRING']))
-			$url .= '?' . $_SERVER['QUERY_STRING'];
-?>
-<html>
-<body>
-Please, wait for page content is being preloaded...<br/>
-<a href="./<?php echo $url; ?>" id="redirect">Continue</a><br/>
-<script type="text/javascript">
-	document.location = './<?php echo $url; ?>';
-	var el = document.getElementById('redirect');
-	el.parentElement.removeChild(el);
-</script>
-</body>
-</html>
-<?php
+		header('Location: ./' . (empty($_SERVER['QUERY_STRING']) ? '' : '?' . $_SERVER['QUERY_STRING']));
 	}
 }
